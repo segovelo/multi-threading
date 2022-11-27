437ad6a74b89f4d291d75d10eccbe4b9433bb71d
segovelo <gauchosinpingo@gmail.com>
2022-11-27 00:49:38 +0000

    Initial commit.

---
+++ src/main/java/com/segovelo/multithreading/MultiThreadingApplication.java
@@ -0,0 +1,13 @@
+package com.segovelo.multithreading;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.SpringBootConfiguration;
+
+@SpringBootConfiguration
+public class MultiThreadingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultiThreadingApplication.class, args);
+	}
+
+}
